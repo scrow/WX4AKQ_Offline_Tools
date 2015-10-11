6c92e2101ff5cd970c0ea5441a479c596e6b6d0c
Steve Crow <scrow@sdf.org>
2015-10-11 19:11:53 -0400

    update mkrelease.sh

--- mkrelease.sh
+++ mkrelease.sh
@@ -3,6 +3,5 @@ if [[ -z "$1" ]]; then
 	echo Release version string not specified
 	echo Usage:  $0 1.0.2-rc2
 else
-	zip -9o "wx4akq-winlink-forms-$1.zip" *.md *.html *.txt
-	zip -9o "wx4akq-winlink-forms-$1+runlocal.zip" *.md *.php *.html *.SAMPLE *.txt Vagrant/* Vagrantfile --exclude config.inc.php
+	zip -9o "wx4akq-winlink-forms-$1.zip" *.md forms/*.html forms/*.txt *.php config.inc.php.SAMPLE Vagrant/* Vagrantfile
 fi
