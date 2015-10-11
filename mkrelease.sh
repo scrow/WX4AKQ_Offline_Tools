5e911f425aaeec0c037b7cf2a1dca708b88ee355
Steve Crow <scrow@sdf.org>
2015-10-11 16:08:35 -0400

    Update mkrelease.sh to build runlocal bundle

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,5 +4,5 @@ if [[ -z "$1" ]]; then
 	echo Usage:  $0 1.0.2-rc2
 else
 	zip -9o "wx4akq-winlink-forms-$1.zip" *.md *.html *.txt
+	zip -9o "wx4akq-winlink-forms-$1+runlocal.zip" *.md *.php *.html *.SAMPLE *.txt Vagrant/* Vagrantfile --exclude config.inc.php
 fi
-
