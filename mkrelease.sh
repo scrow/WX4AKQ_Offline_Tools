e2b1dd75c18c8ee8402abfcfafd64c9296163894
Steve Crow <scrow@sdf.org>
2015-10-13 23:46:26 -0400

    update mkrelease.sh with new project name

--- mkrelease.sh
+++ mkrelease.sh
@@ -3,5 +3,12 @@ if [[ -z "$1" ]]; then
 	echo Release version string not specified
 	echo Usage:  $0 1.0.2-rc2
 else
-	zip -9o "wx4akq-winlink-forms-$1.zip" *.md forms/*.html forms/*.txt *.php config.inc.php.SAMPLE Vagrant/* Vagrantfile
+	newver=wx4akq-offline-tools-$1
+	currdir=`pwd`
+	cd ..
+	cp -R "$currdir" "$newver"
+	zip -9o "$newver.zip" $newver/* --exclude .* queue config.xml mkrelease.sh
+	rm -Rf "$newver"
+	mv "$newver.zip" "$currdir" 
+	cd $currdir
 fi
