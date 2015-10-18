4e471e767a2cc275792eb125559fcd5dab5360f9
Steve Crow <scrow@sdf.org>
2015-10-17 23:07:53 -0400

    add form installer script

---
+++ install_rms_forms.bat
@@ -0,0 +1,24 @@
+@REM 
+@REM 
+@REM #	This file is part of WX4AKQ Offline Tools
+@REM #	
+@REM #	Copyright (c) 2015, Steve Crow, Reid Barden
+@REM #	Licensed under the BSD 2-clause “Simplified” License
+@REM #	
+@REM #	For license information, see the LICENSE.md file or visit
+@REM #	http://wx4akq.org/software
+@REM 
+@REM 
+@echo off
+if not exist "c:\rms express\global folders\templates" goto errormsg
+copy /y forms/*.txt "c:\rms express\global folders\templates"
+copy /y forms/*.html "c:\rms express\global folders\templates"
+goto end
+
+:errormsg
+echo RMS Express not installed or installed to non-standard folder.
+echo Please see the README.md file for manual installation instructions.
+pause
+
+:end
+
