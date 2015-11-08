1978c07d9795623a8560a01c42cbfe063fc1398d
Steve Crow <scrow@sdf.org>
2015-11-07 21:56:25 -0500

    cleanup mkrelease.sh

--- mkrelease.sh
+++ mkrelease.sh
@@ -9,16 +9,17 @@
 #	http://wx4akq.org/software
 #	
 
+
 if [[ -z "$1" ]]; then
 	echo Release version string not specified
 	echo Usage:  $0 1.0.2-rc2
 else
 	newver=wx4akq-offline-tools-$1
-	currdir=`pwd`
+	CURRDIR=`pwd`
 	cd ..
-	cp -R "$currdir" "$newver"
+	cp -R "$CURRDIR" "$newver"
 	zip -9 "$newver.zip" $newver/*.md $newver/*.php $newver/utils/* $newver/*.bat $newver/start_server.sh $newver/Vagrant/bootstrap.sh $newver/Vagrantfile * $newver/resources/* $newver/data/.htaccess $newver/forms/* $newver/files/.htaccess
 	rm -Rf "$newver"
-	mv "$newver.zip" "$currdir" 
-	cd $currdir
+	mv "$newver.zip" "$CURRDIR" 
+	cd $CURRDIR
 fi
