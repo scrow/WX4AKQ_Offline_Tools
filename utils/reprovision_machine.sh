b517cf9432b2d2b578f12e76ddfd346d40dda60a
Steve Crow <scrow@sdf.org>
2015-11-07 21:59:24 -0500

    add reprovision_machine.sh

---
+++ utils/reprovision_machine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#	
+#	This file is part of WX4AKQ Offline Tools
+#	
+#	Copyright (c) 2015, Steve Crow, Reid Barden
+#	Licensed under the BSD 2-clause “Simplified” License
+#	
+#	For license information, see the LICENSE.md file or visit
+#	http://wx4akq.org/software
+#	
+
+CURRDIR=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+cd ..
+
+vagrant halt
+vagrant destroy -f
+vagrant up
+
+cd $CURRDIR
+
