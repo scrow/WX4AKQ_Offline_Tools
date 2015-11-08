057d70507ae20c15998c12bb1324eaba820e2e8e
Steve Crow <scrow@sdf.org>
2015-11-07 21:59:33 -0500

    add reprovision_machine.bat

---
+++ utils/reprovision_machine.bat
@@ -0,0 +1,18 @@
+@REM 
+@REM 
+@REM #	This file is part of WX4AKQ Offline Tools
+@REM #	
+@REM #	Copyright (c) 2015, Steve Crow, Reid Barden
+@REM #	Licensed under the BSD 2-clause “Simplified” License
+@REM #	
+@REM #	For license information, see the LICENSE.md file or visit
+@REM #	http://wx4akq.org/software
+@REM 
+@REM 
+@echo off
+chdir "%~dp0"
+chdir ..
+
+vagrant halt
+vagrant destroy -f
+vagrant up
