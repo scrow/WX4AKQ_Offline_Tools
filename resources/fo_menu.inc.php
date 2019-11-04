e2fc52f5a7dfd6b8a8ffc0bfa8a6938a67c1b92f
steve crow <scrow@stevecrow.net>
2019-11-03 22:25:36 -0500

    Add support for multi-user system bulletin message

--- resources/fo_menu.inc.php
+++ resources/fo_menu.inc.php
@@ -22,10 +22,14 @@
 
 <hr/>
 
+<?php
+if($mesh_mode && file_exists('bulletins/front_page.htm')) {
+	echo(file_get_contents('bulletins/front_page.htm'));
+} else { ?>
 <P>The WX4AKQ Offline Tools package provides special functionality for both SKYWARN Spotters and Net Control Operators in the Wakefield County Warning Area (CWA).  The files in the <code>forms/</code> folder can be used with the RMS Express software package to transmit reports and net log entries over the Winlink radio messaging system.  See the bundled <code>README.md</code> file for setup instructions.</P>
 	
 <P>Users temporarily without Internet access can create Spotter reports and log entries through this web interface to be queued and uploaded to the SKYWARN system once Internet connectivity is available.</P>
-
+<?php };?>
 <hr>
 
 <?php
