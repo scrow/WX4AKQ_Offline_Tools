9c45817ea73237138aa92ebc075665e01e5d3199
steve crow <scrow@stevecrow.net>
2019-11-03 21:43:15 -0500

    Include server shutdown scripts

---
+++ stop_server.bat
@@ -0,0 +1,17 @@
+@REM 
+@REM 
+@REM #	This file is part of WX4AKQ Offline Tools
+@REM #	
+@REM #	Copyright (c) 2015-19, Steve Crow, Reid Barden
+@REM #	Licensed under the BSD 2-clause “Simplified” License
+@REM #	
+@REM #	For license information, see the LICENSE.md file or visit
+@REM #	http://wx4akq.org/software
+@REM 
+@REM 
+@echo off
+chdir "%~dp0"
+
+echo Working.  This may take several mintues...
+vagrant halt
+
