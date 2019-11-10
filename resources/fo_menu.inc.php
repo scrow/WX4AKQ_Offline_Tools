dea7a56745b796b09b851cc9766a5ae95087c065
steve crow <scrow@stevecrow.net>
2019-11-10 08:38:26 -0500

    Display MOTD even if not in mesh mode

--- resources/fo_menu.inc.php
+++ resources/fo_menu.inc.php
@@ -52,7 +52,7 @@ echo('</P>');
 ?>
 
 <?php
-if($mesh_mode && file_exists('data/offline_motd.txt') && strlen(trim(file_get_contents('data/offline_motd.txt')))>8) {
+if(file_exists('data/offline_motd.txt') && strlen(trim(file_get_contents('data/offline_motd.txt')))>8) {
 	echo('<TEXTAREA READONLY style="width: 99%; margin-left: 0px; margin-right: 0px; height: 100px; overflow-y: scroll; resize: vertical">' . "\r\n" . strip_tags(trim(file_get_contents('data/offline_motd.txt'))) . '</TEXTAREA>');
 } else {
 	echo('<HR/>');
